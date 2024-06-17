a30a12d1e74cf7d2e1aa01537b5a7920952aa5bf
Tlaeld <z398438849@outlook.com>
2024-06-17 12:20:30 +0800

    使用helloworld代替clash

--- scripts/diy.sh
+++ scripts/diy.sh
@@ -2,3 +2,5 @@
 
 #更改默认地址为192.168.6.1
 sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
+sed -i "/helloworld/d" "feeds.conf.default"
+echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
