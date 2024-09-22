4a773771a646ee0602dad3450017b7faa8c2da43
Tlaeld <52810644+Tlaeld@users.noreply.github.com>
2024-09-22 14:57:03 +0800

    Update diy.sh

--- scripts/diy.sh
+++ scripts/diy.sh
@@ -3,5 +3,5 @@
 #更改默认地址为192.168.6.1
 sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
 sed -i "/helloworld/d" "feeds.conf.default"
-sed -i '1i src-git passwall https://github.com/xiaorouji/openwrt-passwall.git' "feeds.conf.default"
-sed -i '1i src-git helloworld https://github.com/xiaorouji/openwrt-passwall-packages.git' "feeds.conf.default"
+sed -i '1i src-git-full passwall https://github.com/xiaorouji/openwrt-passwall.git' "feeds.conf.default"
+sed -i '1i src-git-full helloworld https://github.com/xiaorouji/openwrt-passwall-packages.git' "feeds.conf.default"
